4e086101f3d349a9f790fe7ad9443f6854d935c2
Nak <khat.ratanak@cloudnet.com.kh>
2018-03-30 11:32:38 +0700

    Nak upload System

--- erp/config/database.php
+++ erp/config/database.php
@@ -62,7 +62,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '192.168.1.246',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'msk_travel',
